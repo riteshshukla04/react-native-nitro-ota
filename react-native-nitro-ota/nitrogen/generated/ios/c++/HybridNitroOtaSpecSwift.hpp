6598617348ac852a3540347c6a514efe36062fd2
riteshshukla04 <riteshshukla2381@gmail.com>
2025-11-12 22:49:23 +0530

    feat

--- react-native-nitro-ota/nitrogen/generated/ios/c++/HybridNitroOtaSpecSwift.hpp
+++ react-native-nitro-ota/nitrogen/generated/ios/c++/HybridNitroOtaSpecSwift.hpp
@@ -35,9 +35,14 @@ namespace margelo::nitro::nitroota {
   class HybridNitroOtaSpecSwift: public virtual HybridNitroOtaSpec {
   public:
     // Constructor from a Swift instance
-    explicit HybridNitroOtaSpecSwift(const NitroOta::HybridNitroOtaSpec_cxx& swiftPart):
-      HybridObject(HybridNitroOtaSpec::TAG),
-      _swiftPart(swiftPart) { }
+    explicit HybridNitroOtaSpecSwift(NitroOta::HybridNitroOtaSpec_cxx swiftPart)
+      : HybridObject(HybridNitroOtaSpec::TAG)
+      #if DEBUG
+            , _swiftPart(std::move(swiftPart))
+      #else
+            , _swiftPart(swiftPart)
+      #endif
+    { }
 
   public:
     // Get the Swift part
