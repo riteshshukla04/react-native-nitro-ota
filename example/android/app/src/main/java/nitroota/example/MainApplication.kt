e4787c05c7915e7fe7419b2d8590e85e94d15078
riteshshukla04 <riteshshukla2381@gmail.com>
2026-02-28 22:18:32 +0530

    chore: rn-upgrade

--- example/android/app/src/main/java/nitroota/example/MainApplication.kt
+++ example/android/app/src/main/java/nitroota/example/MainApplication.kt
@@ -5,40 +5,26 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.margelo.nitro.nitroota.NitroOta
 import com.margelo.nitro.nitroota.core.getStoredBundlePath
+
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
-
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-        override fun getJSBundleFile(): String? {
-          return getStoredBundlePath(this@MainApplication)
-        }
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+  override val reactHost: ReactHost by lazy {
+    getDefaultReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        },
+        jsBundleFilePath = getStoredBundlePath(applicationContext)
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
   }
-
 }
